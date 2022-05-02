6febdc809deddf7e59c074dca3fd62cd3daff321
john wynne <wynnej1@gamatc.matc.edu>
2022-05-01 23:37:26 -0500

    finally working

---
+++ howstheweather/src/App.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+
+  //const url = "https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=ff6258a2a546289b180cd5da0a2e4921";
+  
+  
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className='humidity'>
+          <p>20%</p>
+          </div>
+          <div className='wind'>
+          12MPH
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
